61c03b67deabf8e2e974cda25f5f1e059261df03
Haru <alfiansahrul267@gmail.com>
2024-09-20 09:51:11 +0700

    dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,86 @@
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+    <div class="row">
+        <div class="col-4">
+            <div class="info-box">
+                <span class="info-box-icon bg-info"><i class="far fa-envelope"></i></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">Messages</span>
+                    <span class="info-box-number">1,410</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="info-box">
+                <span class="info-box-icon bg-info"><i class="far fa-envelope"></i></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">Messages</span>
+                    <span class="info-box-number">1,410</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="info-box">
+                <span class="info-box-icon bg-info"><i class="far fa-envelope"></i></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">Messages</span>
+                    <span class="info-box-number">1,410</span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="card card-outline card-dark">
+        <div class="card-header">
+            <h2 class="card-title">Data Terbaru</h2>
+            <div class="card-tools">
+            </div>
+        </div>
+        <div class="card-body">
+            <!-- Tabel DataTables -->
+            <table id="example" class="table table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th>Company</th>
+                        <th>Contact</th>
+                        <th>Country</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Alfreds Futterkiste</td>
+                        <td>Maria Anders</td>
+                        <td>Germany</td>
+                    </tr>
+                    <tr>
+                        <td>Centro comercial Moctezuma</td>
+                        <td>Francisco Chang</td>
+                        <td>Mexico</td>
+                    </tr>
+                    <tr>
+                        <td>Ernst Handel</td>
+                        <td>Roland Mendel</td>
+                        <td>Austria</td>
+                    </tr>
+                    <tr>
+                        <td>Island Trading</td>
+                        <td>Helen Bennett</td>
+                        <td>UK</td>
+                    </tr>
+                    <tr>
+                        <td>Laughing Bacchus Winecellars</td>
+                        <td>Yoshi Tannamuri</td>
+                        <td>Canada</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+
+    </div>
+
+
 @stop
 
 @section('css')
@@ -16,5 +95,11 @@
 @stop
 
 @section('js')
-    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
+    <!-- Inisialisasi DataTables -->
+    <script src="{{ asset('vendor/datatables/datatables.min.js') }}"></script>
+    <script>
+        $(document).ready(function() {
+            $('#example').DataTable(); // Inisialisasi DataTables untuk tabel dengan id "example"
+        });
+    </script>
 @stop
